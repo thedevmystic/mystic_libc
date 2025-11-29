19949d2602f3bde8b03b1c0955ae1fb8a6fa8161
Surya <thedevmystic@gmail.com>
2025-11-29 19:25:38 +0530

    Addition: Added crti.s for x86.

--- crt/x86/crti.s
+++ crt/x86/crti.s
@@ -1,28 +1,71 @@
 /**
- * MysticLibc - Standard C Library.
+ * Copyright 2025 Suryansh Singh
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @file crti.s (i386)
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- * @brief Prolouge of C Runtime Routine.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * @path [ROOT]/crt/x86/crti.s
+ * @file crti.s
+ * @brief Initialization start of C Runtime Routine.
+ *
+ * @details
+ * This header file contains _init, and _fini sections of a C program.
+ * It provides essential initialization startup routine of the core
+ * C Runtime Routine.
+ *
+ * @sa [ROOT]/crt/x86/crt1.s - Entry point of C Runtime Routine
+ * @sa [ROOT]/crt/x86/crtn.s - The initialization end of C Runtime
+ * @sa [ROOT]/crt/crtbegin.c - The initialization of constructors and destructor
+ *                             of C Runtime (GNU C feature, or C++ feature)
+ * @sa [ROOT]/crt/crtend.c   - The end of constructor and destructor lists of 
+ *                             C Runtime (GNU C feature, or C++ feature)
+ * @sa [ROOT]/crt/crt1.c     - The __start_c, and __libc_start_main functions.
+ *
+ * @author thedevmystic (Surya)
+ * @copyright 2025 Suryansh Singh Apache-2.0 License
+ *
+ * SPDX-FileCopyrightText: 2025 Suryansh Singh
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-.code32                          /* Generate 32 bits instructions */
+/**
+ * @brief Generate 32 bits instructions.
+ */
+.code32
 
+/**
+ * @brief Prologue initialization section for constructors.
+ */
 .global _init
-.global _fini
-
-/* Prolouge section of _init */
 .section .init, "ax"
 .type _init, @function
 .align 16
 _init:
-    pushl %ebp              /* Push EBP register */
-    movl  %esp, %ebp        /* Move ESP to EBP */
+    /* Push EBP register */
+    pushl %ebp
+    /* Move ESP to EBP */
+    movl  %esp, %ebp
 
-/* Prolouge section of _fini */
+/**
+ * @brief Prologue initialization section for destructors.
+ */
+.global _fini
 .section .fini, "ax"
 .type _fini, @function
 .align 16
 _fini:
-    pushl %ebp              /* Push EBP register */
-    movl  %esp, %ebp        /* Move ESP to EBP */
+    /* Push EBP register */
+    pushl %ebp
+    /* Move ESP to EBP */
+    movl  %esp, %ebp
+
