2614114d1bd7a4dcb845237aed61ab1db765c976
TheDevMystic <thedevmystic@gmail.com>
2025-12-10 10:50:40 +0530

    Addition: Added crti.s for arm32.

--- crt/arm32/crti.s
+++ crt/arm32/crti.s
@@ -1,28 +1,76 @@
 /**
- * MysticLibc - Standard C Library.
- * 
- * @file crti.s (arm32)
+ * Copyright 2025 Suryansh Singh
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * @brief C Runtime Routine Prologue.
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * @path [ROOT]/crt/arm32/crti.s
+ * @file crti.s
+ * @brief Initialization start of C Runtime Routine.
+ *
+ * @details
+ * This header file contains _init, and _fini start sections of a C program.
+ * It provides essential initialization startup routine of the core
+ * C Runtime Routine.
+ *
+ * @sa [ROOT]/crt/arm32/crt1.s - Entry point of C Runtime Routine
+ * @sa [ROOT]/crt/arm32/crtn.s - The initialization end of C Runtime
+ * @sa [ROOT]/crt/crtbegin.c - The initialization of constructors and destructor
+ *                             of C Runtime (GNU C feature, or C++ feature)
+ * @sa [ROOT]/crt/crtend.c   - The end of constructor and destructor lists of 
+ *                             C Runtime (GNU C feature, or C++ feature)
+ * @sa [ROOT]/crt/crt1.c     - The __start_c, and __libc_start_main functions.
+ *
+ * @author thedevmystic (Surya)
+ * @copyright 2025 Suryansh Singh Apache-2.0 License
+ *
+ * SPDX-FileCopyrightText: 2025 Suryansh Singh
+ * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @brief Generate 32 bits instructions.
+ */
+.code32
+
+/**
+ * @brief Use unified syntax (thumb + arm).
+ */
 .syntax unified
 
+/**
+ * @brief Prologue section of init.
+ */
 .global _init
-.global _fini
-
-/* Prologue section of init */
 .section .init, "ax"
 .type _init, @function
 .align 2
 _init:
-    push {r11, lr}            /* Save frame pointer and return address */
-    mov  r11, sp              /* Set frame pointer (r11 = sp) */
+    /* Save frame pointer and return address */
+    push {r11, lr}
+    /* Set frame pointer (r11 = sp) */
+    mov  r11, sp
 
-/* Prologue section of fini */
+/**
+ * @brief Prologue section of fini.
+ */
+.global _fini
 .section .fini, "ax"
 .type _fini, @function
 .align 2
 _fini:
-    push {r11, lr}            /* Save frame pointer and return address */
-    mov  r11, sp              /* Set frame pointer (r11 = sp) */
+    /* Save frame pointer and return address */
+    push {r11, lr}
+    /* Set frame pointer (r11 = sp) */
+    mov  r11, sp
+
